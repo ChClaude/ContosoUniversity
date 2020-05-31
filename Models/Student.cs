d539163a513ddd856d40871a51f7a35382cef33e
ChClaude <christ.tchambila@gmail.com>
2020-06-01 00:18:50 +0200

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required] // this must be used with the minimum length, specified after this line
+        [StringLength(50, MinimumLength = 2)] // MinimumLength and Required allow whitespace to satisfy the validation. Use the RegularExpression attribute for full control over the string.
+        [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")] // requires the first character to be upper case and the remaining characters to be alphabetical
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")] // requires the first character to be upper case and the remaining characters to be alphabetical 
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        
+        // FullName is a calculated property that returns a value that's created by concatenating two other properties.
+        // FullName can't be set, so it has only get accessor. No FullName column is created in the database.
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
